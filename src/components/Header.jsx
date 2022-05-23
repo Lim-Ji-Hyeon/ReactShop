51b52c802ed4cd0b9a5b7c60ee57953354d3df58
Lim-Ji-Hyeon <sen03620@naver.com>
2022-05-23 17:03:25 +0900

    프로젝트 세팅

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/header.css'
+
+export default function Header() {
+  return (
+  <>
+   
+  </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App'
+import './index.css'
+import Header from './components/Header'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+)
