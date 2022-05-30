9b91f5534597b1d75362b147e54d04ecc85863cd
Alice Lee <alice@Aliceui-MacBookPro.local>
2022-05-30 22:02:31 +0900

    Feat : 카로셀 컴포넌트 구현

---
+++ src/components/data/CarouselDummy.js
@@ -0,0 +1,7 @@
+export const image = [
+  'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+  'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+  'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+  'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+  'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+]
