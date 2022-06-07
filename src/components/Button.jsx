02dbed7db32d1c7891f945b72cc12e7b4bdbb539
Lim-Ji-Hyeon <sen03620@naver.com>
2022-06-07 17:50:36 +0900

    Refactor : 버튼 컴포넌트 내부 함수 삭제 및 코드 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,55 +6,18 @@ export default function Button({type, color="purple" ,borderRadius="all", hoverC
   )
 }
 
-const sizeFunction = (type, theme) => {
-  switch (type) {
-    case "xSmall" :
-      return theme.width.xSmall
-    case "small" :
-      return theme.width.small
-    case "medium" :
-      return theme.width.medium
-    case "large" :
-      return theme.width.large
-  }
-}
-
-const radiusFunction = (borderRadius, theme) => {
-  switch (borderRadius) {
-    case "all" :
-      return theme.borderRadius.all
-    case "left" :
-      return theme.borderRadius.left
-    case "right" :
-      return theme.borderRadius.right
-  }
-}
-
-const colorFunction = (color, theme) => {
-  switch (color) {
-    case "purple" :
-      return theme.color.purple
-    case "black" :
-      return theme.color.black
-    case "white" :
-      return theme.color.white
-    case "grey" :
-      return theme.color.grey
-  }
-}
-
 const StyledButton = styled.button`
-  width : ${({theme, type}) => sizeFunction(type, theme)};
+  width : ${({theme, type}) => theme.width[type]};
   height : ${({theme}) => theme.height.medium};
-  background-color : ${({theme, color}) => colorFunction(color, theme)};
+  background-color : ${({theme, color}) => theme.color[color]};
   color : ${({theme}) => theme.color.white};
   font-size : ${({theme}) => theme.font.size.normal};
   font-weight : ${({theme}) => theme.font.weight.normal};
   text-align : center;
   cursor : pointer;
   border : 0;
-  border-radius : ${({theme, borderRadius}) => radiusFunction(borderRadius, theme)};
+  border-radius : ${({theme, borderRadius}) => theme.borderRadius[borderRadius]};
   &:hover {
-    background-color : ${({theme, hoverColor}) => colorFunction(hoverColor, theme)};
+    background-color : ${({theme, hoverColor}) => theme.color[hoverColor]};
   }
 `
