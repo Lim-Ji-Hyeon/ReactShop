14c843b93780323a162a16ed26155ed697ed9a60
Alice Lee <alice@Aliceui-MacBookPro.local>
2022-06-08 14:43:01 +0900

    Feat : MainPage에 Card 추가

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,8 +1,29 @@
-import React from "react"
-
+import React, { useEffect, useState } from 'react'
+import { CardSection } from '../components/card'
+import { GET } from '../api/axios'
 export default function MainPage() {
+  const [newProducts, setNewProducts] = useState([])
+
+  const getNewProducts = async () => {
+    const products = await Promise.all([
+      GET('/products/category/electronics'),
+      GET('/products/category/jewelery'),
+      GET("/products/category/men's clothing"),
+      GET("/products/category/women's clothing"),
+    ]).then((res) => {
+      return res.map(
+        (category) => category[category.length - 1]
+      )
+    })
+    await setNewProducts(products)
+  }
+
+  useEffect(() => {
+    getNewProducts()
+  }, [])
   return (
-    <h1>MainPage</h1>
+    <>
+      <CardSection title='신제품' data={newProducts} />
+    </>
   )
 }
-
