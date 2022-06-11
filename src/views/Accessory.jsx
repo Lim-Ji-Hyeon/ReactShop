a4bdec04a691b8465aa083f20de61f6c479137c3
Lim-Ji-Hyeon <sen03620@naver.com>
2022-06-11 20:27:32 +0900

    Refactor : 액세서리 페이지 추가

--- src/views/Accessory.jsx
+++ src/views/Accessory.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BreadCrumbs from "../components/BreadCrumbs";
+import { CardSection } from '../components/card'
+import { GET } from '../api/axios'
 
 export default function Accessory() {
+  const [accessory, setAccessory] = useState([])
+
+  const getAccessory = async () => {
+    const products = await Promise.all([GET('/products/category/jewelery')]).then((res) => {return res[0]})
+    await setAccessory(products)
+    }
+
+  useEffect(() => {
+    getAccessory()
+  }, [])
+
   return (
-    <h1>AccessaryPage</h1>
+    <>
+      <BreadCrumbs from={"홈"} to={"액세서리"}/>
+      <CardSection title={"액세서리"} data={accessory}/>
+    </>
   )
 }
