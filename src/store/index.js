86f8aeca1fb8a523533c07119c36ddadcc97608e
Lim-Ji-Hyeon <sen03620@naver.com>
2022-06-11 20:04:32 +0900

    Refactor : Redux 코드 추가

--- src/store/index.js
+++ src/store/index.js
@@ -7,10 +7,14 @@ export const initialState = [
   {productList : [],},
 ]
 
+export const headerInit = [
+  {headerMode : "black",},
+]
 
 export const incrementCartNumber = createAction('Increment_Cart_Number')
 export const addCart = createAction('ADD_CART')
 export const purchase = createAction('PURCHASE')
+export const headerModeChange = createAction('Mode_Change') 
 
 const cartReducer = createReducer(initialState, {
   [addCart] : (state, {payload}) => {state[1].productList.push(payload.id)},
@@ -18,7 +22,11 @@ const cartReducer = createReducer(initialState, {
   [incrementCartNumber] : (state, {payload}) => {state[0] = {cartNumber : payload.number += 1}},
 })
 
-const reducer = combineReducers({cartReducer})
+const headerReducer = createReducer(headerInit, {
+  [headerModeChange] : (state, {payload}) => {state[0] = {headerMode : payload.store}},
+})
+
+const reducer = combineReducers({cartReducer, headerReducer})
 
 const persistConfig = {
   key : 'root',
