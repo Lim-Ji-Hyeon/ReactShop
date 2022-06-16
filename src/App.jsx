1015aef4fb24d0099b64c6f4343e8c5e06a1bf1c
Alice Lee <alice@Aliceui-MacBookPro.local>
2022-06-16 15:53:53 +0900

    Refactor: path / 추가

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
             <Routes>
               <Route path={'/'} element={<MainPage />} />
               <Route
-                path={'fashion'}
+                path={'/fashion'}
                 element={<Fashion />}
               />
               <Route
