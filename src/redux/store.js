f5ef5977b3ac32fe9f2a45c9f9d4e01698f6def1
Lim-Ji-Hyeon <sen03620@naver.com>
2022-06-21 21:25:38 +0900

    Feat : 상품 데이터 캐싱 Redux 파일 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import React from "react";
 import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import productReducer from './product'
 import modeReducer from './mode'
+import setProductReducer from './setProduct'
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 // import logger from "redux-logger";
@@ -10,6 +11,7 @@ import storage from "redux-persist/lib/storage";
 const reducers = combineReducers({
   product : productReducer,
   mode : modeReducer,
+  setProduct : setProductReducer,
 })
 
 const persistConfig = {
