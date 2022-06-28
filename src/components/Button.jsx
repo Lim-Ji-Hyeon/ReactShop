c45fd7c88e44ddf3cdcdcfa1dd2848a4bd5f6713
Alice Lee <alice@Aliceui-MacBookPro.local>
2022-06-28 17:35:09 +0900

    Refactor : 필요한 props 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,57 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-export default function Button({size, color="purple" ,borderRadius="all", hoverColor="green" ,children, onClick}) {
+export default function Button({
+  id,
+  type = "button",
+  size,
+  color = "purple",
+  borderRadius = "all",
+  hoverColor = "green",
+  children,
+  onClick,
+  dataId,
+  margin,
+  position,
+  top,
+  bottom,
+  right,
+  left
+}) {
   return (
-    <StyledButton onClick={onClick} size={size} color={color} hoverColor={hoverColor} borderRadius={borderRadius}>{children}</StyledButton>
+    <StyledButton
+      type={type}
+      id={id}
+      onClick={onClick}
+      size={size}
+      color={color}
+      hoverColor={hoverColor}
+      borderRadius={borderRadius}
+      data-id={dataId}
+      margin={margin}
+    >
+      {children}
+    </StyledButton>
   )
 }
 
 const StyledButton = styled.button`
-  width : ${({theme, size}) => theme.width[size]};
-  height : ${({theme}) => theme.height.medium};
-  background-color : ${({theme, color}) => theme.color[color]};
-  color : ${({theme}) => theme.color.white};
-  font-size : ${({theme}) => theme.font.size.normal};
-  font-weight : ${({theme}) => theme.font.weight.normal};
-  text-align : center;
-  cursor : pointer;
-  border : 0;
-  border-radius : ${({theme, borderRadius}) => theme.borderRadius[borderRadius]};
+  margin: ${({ margin }) => margin};
+  width: ${({ theme, size }) => theme.width[size]};
+  height: ${({ theme }) => theme.height.medium};
+  position: ${({ position }) => position};
+  top: ${({ top }) => top};
+  bottom: ${({ bottom }) => bottom};
+  right: ${({ right }) => right};
+  left: ${({ left }) => left};
+  background-color: ${({ theme, color }) => theme.color[color]};
+  color: ${({ theme }) => theme.color.white};
+  font-size: ${({ theme }) => theme.font.size.normal};
+  font-weight: ${({ theme }) => theme.font.weight.normal};
+  text-align: center;
+  cursor: pointer;
+  border: 0;
+  border-radius: ${({ theme, borderRadius }) => theme.borderRadius[borderRadius]};
   &:hover {
-    background-color : ${({theme, hoverColor}) => theme.color[hoverColor]};
+    background-color: ${({ theme, hoverColor }) => theme.color[hoverColor]};
   }
 `
